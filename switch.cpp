818e81e03aabb9b37691507ad8c18c235cba5368
jei <jeichen00@gmail.com>
2016-03-17 21:19:18 -0700

    test

--- switch.cpp
+++ switch.cpp
@@ -8,6 +8,6 @@ int main()
 	case 2 : cout << num << ": Tuesday" ; break;
 	case 3 : cout << num << ": Wednesday" ; break;
 	case 4 : cout << num << ": Thursday" ; break;
-	case 5 : cout << num << ": Friay" ; break;
+	case 5 : cout << num << ": Friaay" ; break;
 	default : cout << num << ": Monday" ; break;
 }
